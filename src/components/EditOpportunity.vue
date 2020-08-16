5ea777961d4f5b669f0c9a363962d60336181dc1
sigma0wl <ruizjoseph17@gmail.com>
2020-08-16 10:27:23 -0400

    Edit Opportunities Working

--- src/components/EditOpportunity.vue
+++ src/components/EditOpportunity.vue
@@ -12,14 +12,18 @@
             </v-card-title>
             <v-card-text>
                 <v-form>
-                    <v-text-field cols="2" name="add-opp-name" placeholder="Center Name" :value="parentData.name"></v-text-field>
-                    <v-date-picker name="center-date"></v-date-picker>
+                    <v-text-field cols="2" 
+                    name="add-opp-name" 
+                    placeholder="Center Name" 
+                    :value="parentData.name" 
+                    v-model="opp_name"></v-text-field>
+                    <v-date-picker name="center-date" v-model="opp_date"></v-date-picker>
                 </v-form>
             </v-card-text>
             <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="blue darken-1" text @click="edit_dialog=false">Close</v-btn>
-                <v-btn color="blue darken-1" text @click="test">Save</v-btn>
+                <v-btn color="blue darken-1" text @click="saveNewOpportunity">Save</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
@@ -28,14 +32,25 @@
 <script>
     export default {
         name: "EditOpportunity",
+        methods: {
+            closeDialog(){
+                this.edit_dialog = false;
+            },
+            saveNewOpportunity(){
+                this.parentData.name = this.opp_name;
+                this.parentData.date = this.opp_date;
+                this.closeDialog();
+            }
+        },
         props:{
             parentData: Object,
             edit: Boolean,
         },
         data(){
             return{
-                edit_dialog : false
-            }
+                edit_dialog : false,
+                opp_name: this.parentData.name,
+            } 
         }
     }
 </script>
